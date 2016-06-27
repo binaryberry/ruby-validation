c2eef176bdae7d23669c9ef6045cdda3d0e122b5
Ben Hyland <ben.hyland@digital.cabinet-office.gov.uk>
2016-06-27 17:57:22 +0100

    add errors

--- lib/nel/non_empty_list.rb
+++ lib/nel/non_empty_list.rb
@@ -1,20 +1,26 @@
 class NonEmptyList
 
   def initialize(first_element, *other_elements)
+    raise "not implemented"
   end
 
   def append(element)
+    raise "not implemented"
   end
 
   def prepend(element)
+    raise "not implemented"
   end
 
   def append_all(*elements)
+    raise "not implemented"
   end
 
   def prepend_all(*elements)
+    raise "not implemented"
   end
 
   def to_a
+    raise "not implemented"
   end
 end
