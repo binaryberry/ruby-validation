d61e7e050c6f790ee27e97a6653c00c245aa01bb
benhyland <ben.hyland@digital.cabinet-office.gov.uk>
2016-09-19 14:06:06 +0100

    example NEL impl

--- lib/nel/non_empty_list.rb
+++ lib/nel/non_empty_list.rb
@@ -1,42 +1,46 @@
 class NonEmptyList
   # Create a NonEmptyList instance guaranteed to contain at least one element.
   def initialize(first_element, *other_elements)
-    raise "not implemented"
+    if first_element == nil || other_elements == nil || other_elements.include?(nil)
+      raise "NonEmptyList may not contain nil"
+    end
+    @values = [first_element] + other_elements
   end
 
   # Return the first element.
   def head
-    raise "not implemented"
+    @values[0]
   end
 
   # Returns an array containing all elements except the first, in order.
   # Returns an empty array if this NonEmptyList contains only a single element.
   def tail
-    raise "not implemented"
+    @values[1..-1]
   end
 
   # Returns a new NonEmptyList that contains all the elements of this instance, with the given element appended.
   def append(element)
-    raise "not implemented"
+    NonEmptyList.new(head, *(tail << element))
   end
 
   # Returns a new NonEmptyList that contains all the elements of this instance, with the given element prepended.
   def prepend(element)
-    raise "not implemented"
+    NonEmptyList.new(element, *to_a)
   end
 
   # Returns a new NonEmptyList that contains all the elements of this instance, with all the given elements appended in the given order.
   def append_all(*elements)
-    raise "not implemented"
+    NonEmptyList.new(head, *(tail + elements))
   end
 
   # Returns a new NonEmptyList that contains all the elements of this instance, with all the given elements prepended in the given order.
   def prepend_all(*elements)
-    raise "not implemented"
+    new_values = elements + to_a
+    NonEmptyList.new(new_values[0], *(new_values[1..-1]))
   end
 
   # Returns an array containing all the elements of this NonEmptyList in order.
   def to_a
-    raise "not implemented"
+    [head] + tail
   end
 end
